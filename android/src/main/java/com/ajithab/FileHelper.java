b45264fbc68a491a1ecb848675be01151f0bbbbc
Nolan <nolanpotier@gmail.com>
2019-09-05 17:42:36 +0200

    Add getFilePath to get the full file path

--- android/src/main/java/com/ajithab/FileHelper.java
+++ android/src/main/java/com/ajithab/FileHelper.java
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.WritableNativeMap;
 
 import android.net.Uri;
 import android.database.Cursor;
+import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 
 public class FileHelper {
@@ -43,11 +44,36 @@ public class FileHelper {
         return type;
     }
 
+    public String getFilePath(Uri uri) {
+        String[] columns = { MediaStore.Images.Media.DATA };
+        String result = "";
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = this.reactContext.getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int columnIndex;
+                    columnIndex = cursor.getColumnIndex(columns[0]);
+                    result = cursor.getString(columnIndex);
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
     public WritableMap getFileData(Uri uri) {
         WritableMap fileData = new WritableNativeMap();
         fileData.putString("name", this.getFileName(uri));
         fileData.putString("mime", this.getMimeType(uri));
-        fileData.putString("path", uri.toString());
+        fileData.putString("path", "file://" + this.getFilePath(uri));
 
         return fileData;
     }
