0daa7af1228ea0aa1aea85282220cbccb5eae1b6
Andriy Stabryn <andriy.stabryn@agiliway.com>
2019-10-22 15:32:50 +0300

    Changed file types

--- ios/src/RNFileShareIntent.m
+++ ios/src/RNFileShareIntent.m
@@ -20,82 +20,74 @@ static NSExtensionContext* extContext;
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(getFilePath:(RCTResponseSenderBlock)callback)
+RCT_REMAP_METHOD(getFilePath,
+                 getFilePathResolve:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
 {
     if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeURL]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeURL options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeFileURL]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeFileURL]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeFileURL options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypePDF]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypePDF]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypePDF options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeBMP]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeBMP]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeBMP options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeGIF]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeGIF]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeGIF options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeICO]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeICO]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeICO options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeMP3]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeMP3]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeMP3 options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypePNG]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypePNG]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypePNG options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeJPEG]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeJPEG]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeJPEG options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeMPEG]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeMPEG]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeMPEG options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeMPEG4]) {
+        [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeMPEG4 options:nil completionHandler:^(NSURL *url, NSError *error) {
+            resolve(@[url.absoluteString]);
+        }];
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeText]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeText]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeText options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeAudio]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeAudio]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeAudio options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
-    }
-    if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeVideo]) {
+    } else if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeVideo]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeVideo options:nil completionHandler:^(NSURL *url, NSError *error) {
-            callback(@[url.absoluteString]);
+            resolve(@[url.absoluteString]);
         }];
+    } else {
+        NSError *error = [[NSError alloc] initWithDomain:@"com.levuro.engage" code:100 userInfo: @{NSLocalizedDescriptionKey: @"Invalid file type"}];
+        reject(@"invalid_filetype", @"Invalid file type", error);
     }
-    
 }
 
 RCT_EXPORT_METHOD(openURL:(NSString *)url) {
@@ -114,7 +106,8 @@ RCT_EXPORT_METHOD(getFiles:(RCTResponseSenderBlock)callback)
 {
     NSArray *types = @[
                        (NSString *)kUTTypeImage,
-                       (NSString *)kUTTypeContent
+                       (NSString *)kUTTypeMPEG4,
+                       (NSString *)kUTTypeMPEG
                        ];
 
     [self getItems:types withCallback:callback];
